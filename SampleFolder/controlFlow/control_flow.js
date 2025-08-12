17c2642a08e8f691609b8bd2ccf757d875b00863
welsoncj <welsoncj@gmail.com>
2025-08-11 20:30:08 -0400

    add authentication

--- SampleFolder/controlFlow/control_flow.js
+++ SampleFolder/controlFlow/control_flow.js
@@ -4,6 +4,8 @@ let isLoggedIn = true;
 let userMessage;
 let userType = "subscriber";
 let userCategory;
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 if (userRole === "admin") {
     accessLevel = "Full access granted";
@@ -40,3 +42,4 @@ switch (userType) {
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+console.log("Authentication Status:", authenticationStatus);
